4bf06398408c86666942482948d48a6b0ca0a5db
Bispo047 <arthursomotos@gmail.com>
2026-06-23 03:21:58 -0300

    feat: adiciona movimento especial de roque (pequeno e grande)

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -19,6 +23,11 @@ public class King extends ChessPiece {
     private boolean canMove(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -35,6 +44,31 @@ public class King extends ChessPiece {
             p.setValues(position.getRow() + offset[0], position.getColumn() + offset[1]);
             if (getBoard().positionExists(p) && canMove(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+
+        // Roque pequeno (kingside)
+        if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+            Position rookPos = new Position(position.getRow(), position.getColumn() + 3);
+            if (testRookCastling(rookPos)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if (!getBoard().thereIsAPiece(p1) && !getBoard().thereIsAPiece(p2)) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+        }
+
+        // Roque grande (queenside)
+        if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+            Position rookPos = new Position(position.getRow(), position.getColumn() - 4);
+            if (testRookCastling(rookPos)) {
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                if (!getBoard().thereIsAPiece(p1) && !getBoard().thereIsAPiece(p2) && !getBoard().thereIsAPiece(p3)) {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
             }
         }
 
